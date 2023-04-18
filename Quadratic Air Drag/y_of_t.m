ba8fa446002c0d9250a7490f79b12f8c4cae2920
Curran Robertson <100748551+curranjrobertson@users.noreply.github.com>
2023-04-18 00:20:17 -0600

    update quadratic

--- Quadratic Air Drag/y_of_t.m
+++ Quadratic Air Drag/y_of_t.m
@@ -13,6 +13,7 @@ M1 = 343; % Value of Mach 1 (speed of sound) in m/s
 
 % Variables
 step = 0.1; % step size
+t = step:step:t_max;
 
 % Unit Conversions
 th = deg2rad(th); % convert to radians
@@ -20,25 +21,22 @@ th = deg2rad(th); % convert to radians
 % Derived Constants
 voy = vo*sin(th); % initial velocity in the y-direction
 
-vy = voy;
-t = step;
-while t < t_max % while time is less than max time in seconds
-    while vy < M1 % If the velocity in the y-direction is less than Mach 1
-        while vy < 24 % If the velocity in the y-direction is less than 24 m/s
-            vy = -g/c1 + (c1*vo*sin(th) + g).*exp(-c1.*t)./c1; % Linear air drag
-            y = -(g/c1).*t + (c1*vo*sin(th) + g).*(1 - exp(-c1.*t))./c1^2; % Linear air drag
-            t = t + step;
+vy = voy; % Init vy
+
+for t = step:step:t_max
+    if vy < M1 && t < t_max % If the velocity in the y-direction is less than Mach 1
+        if vy < 24 && t < t_max   % If the velocity in the y-direction is less than 24 m/s
+            vy = -g/c1 + (c1*vo*sin(th) + g).*exp(-c1.*t)./c1 % Linear air drag
+            y = -(g/c1).*t + (c1*vo*sin(th) + g).*(1 - exp(-c1.*t))./c1^2 % Linear air drag
         end
-        vy = -g/c2 + (c2*vo*sin(th) + g).*exp(-c2.*t)./c2; % Quadratic air drag (This might be wrong)
-        y = -(g/c2).*t + (c2*vo*sin(th) + g).*(1 - exp(-c2.*t))./c2^2; % Quadratic air drag (this might be wrong)
-        t = t + step;
-    end
-    while  vy >= M1 % If the velocity in the y-direction is greater than Mach 1
-        vy = -g/c1 + (c1*vo*sin(th) + g).*exp(-c1.*t)./c1; % Linear air drag
-        y = -(g/c1).*t + (c1*vo*sin(th) + g).*(1 - exp(-c1.*t))./c1^2; % Linear air drag
-        t = t + step;
+        vy = -g/c1 + (c1*vo*sin(th) + g).*exp(-c1.*t)./c1 % Linear air drag
+        y = -(g/c1).*t + (c1*vo*sin(th) + g).*(1 - exp(-c1.*t))./c1^2 % Linear air drag
+    elseif vy >= M1 && t < t_max  % If the velocity in the y-direction is greater than Mach 1
+        vy = -g/c1 + (c1*vo*sin(th) + g).*exp(-c1.*t)./c1 % Linear air drag
+        y = -(g/c1).*t + (c1*vo*sin(th) + g).*(1 - exp(-c1.*t))./c1^2;% Linear air drag
     end
 end
+
 
 % Plot
 plot(t, y);

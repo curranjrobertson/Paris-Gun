4b3df3ab5bd93cfb34df57090aee519efbbdd22c
Curran Robertson <100748551+curranjrobertson@users.noreply.github.com>
2023-04-17 18:39:16 -0600

    y of x equation

--- No Air Drag/y_of_x.m
+++ No Air Drag/y_of_x.m
@@ -6,6 +6,7 @@ clear all; clc; close all
 vo = 1640; % initial velocity in m/s
 th = 55; % Launch Angle in degrees
 cd = 0.0027; % drag coefficient in 1/s
+g = 9.81; % acceleration due to gravity in m/s^2
 
 % Variables
 step = 0.1; % step size
@@ -14,8 +15,12 @@ x = 0.1:step:130; % time vector in seconds
 % Unit Conversions
 th = deg2rad(th);
 
-% Equation 
-y = 
+% Derived Constants
+voy = vo*sin(th); % initial y velocity in m/s
+vox = vo*cos(th); % initial x velocity in m/s
+
+% Equation Hibbler dynamics Projectile Motion 
+y = voy*(x/vox) - (1/2)*g*(x/vox).^2;
 
 % Plot
 plot(x, y);

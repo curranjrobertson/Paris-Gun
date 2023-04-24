ca3c7c83005a2a7f63818ae97f58ba24e6fa91f2
kfdegross <131399513+kfdegross@users.noreply.github.com>
2023-04-24 10:33:05 -0600

    Update vx_of_x.m

--- No Air Drag/vx_of_x.m
+++ No Air Drag/vx_of_x.m
@@ -1,16 +1,27 @@
-% This script plots the velocity in the x-direction of the projectile as a
-% function of x with no air drag.
+%{
+Velocity in the x-direction as a function of x-position without air drag.
+
+Authors: Curran J. Robertson, Kyden F. DeGross 
+Professor: Dr. Daniel Rederth
+
+Last Modified: 04/24/2023
+Description: Script calculates and plots the velocity in the x-direction of the Paris 
+Gun projectile as function of the x-position without air drag
+
+OUTPUTS: 
+  Figure 1: Velocity of the projectile in the x-direction as a function of the x-position without air drag
+%}
 clear all; clc; close all
 
 % Constants
-vo = 1640; % initial velocity in m/s
-th = 55; % Launch Angle in degrees
-c1 = 0.0027; % drag coefficient in 1/s
-x_max = 130; % maximum x-position in m
+vo = 1640; % initial velocity (m/s)
+th = 55; % Launch Angle (degrees)
+c1 = 0.0027; % drag coefficient (1/s)
+x_max = 130; % maximum x-position (m)
 
 % Variables
 step = 0.1; % step size
-x = step:step:x_max; % time vector in seconds
+x = step:step:x_max; % distance vector in (m)
 
 % Unit Conversions
 th = deg2rad(th);

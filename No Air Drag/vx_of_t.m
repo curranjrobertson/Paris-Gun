0b85c1a9517662ef2430e864089128b80bd5fa6b
kfdegross <131399513+kfdegross@users.noreply.github.com>
2023-04-24 10:10:45 -0600

    Update vx_of_t.m
    Hello Curran I just added a more robust header to the code and changed the x_max to t_max as the 182 is a time of flight measurement in seconds not range in meters. But other than that  I think this is really good.

--- No Air Drag/vx_of_t.m
+++ No Air Drag/vx_of_t.m
@@ -1,17 +1,35 @@
-% This script plots the velocity in the x-direction of the projectile as a function of t
-% with no air drag.
+%{
+
+Velocity in the x Direction as a function of time without air drag
+
+Authors: Curran J. Robertson, Kyden F. DeGross 
+Professor: Dr. Daniel Rederth
+
+Last Modified: 04/24/2023
+
+Description: Script calculates and plots the velocity in the x-direction of the Paris 
+Gun projectile as function of time without air drag
+
+
+CONSTANTS: The real ambient temperature in degrees Celcius (RealTemp) as a
+real number between the range of -86<=RealTemp<=-41.
+
+OUTPUTS: 
+  Figure 1: Velocity of the projectile in the x-direction as a function of time without air drag
+
+%}
 
 clear all; clc; close all
 
 % Constants
-vo = 1640; % initial velocity in m/s
-th = 55; % Launch Angle in degrees
-c1 = 0.0027; % drag coefficient in 1/s
-x_max = 182; % maximum range in m
+vo = 1640; % initial velocity of projectile (m/s)
+th = 55; % Launch Angle (degrees)
+c1 = 0.0027; % drag coefficient of the projectile (1/s)
+t_max = 182; % maximum time of flight (seconds)
 
 % Variables
-step = 0.1; % step size
-t = step:step:x_max; % time vector in seconds
+step = 0.1; % step size (seconds)
+t = step:step:t_max; % time vector (seconds)
 
 % Unit Conversions
 th = deg2rad(th);
@@ -24,7 +42,6 @@ vx = vo_x; % velocity in x direction
 
 % Plot
 plot(t, vx);
-hold on
 title('Velocity in the X-Direction of Projectile as a Function of Time With No Air Drag')
 xlabel('time (s)')
 ylabel('X position (m)')

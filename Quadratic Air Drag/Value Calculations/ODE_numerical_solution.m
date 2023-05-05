25c465f865185f2c018e378606011128c4a3af0b
Curran Robertson <100748551+curranjrobertson@users.noreply.github.com>
2023-05-04 18:00:26 -0600

    Update ODE_numerical_solution.m

--- Quadratic Air Drag/Value Calculations/ODE_numerical_solution.m
+++ Quadratic Air Drag/Value Calculations/ODE_numerical_solution.m
@@ -1,15 +1,18 @@
 % Attempt to solve 24 m/s < Vy < Mach 1 
+
 clear all;clc;close all
+
 % constants
 c2 = 0.0027; % drag coefficient (change)
 g = 9.81; % acceleration due to gravity in m/s^2
 
-tspan = [38.9 88.5]; % timespan in seconds
-yspan = [3.55 4.89]; % y span (change)
+% tspan = [38.9 88.5]; % timespan in seconds
+% yspan = [3.55 4.89]; % y span (change)
 
+tspan = [0 5];
+yspan = [3.55 4.89];
 % tolerance change
-options = odeset('RelTol',1e-8,'AbsTol',1e-10)
-
+options = odeset('RelTol',1e-8,'AbsTol',1e-10);
 
 syms y(t)
 [V] = odeToVectorField(-diff(y,2) == c2*diff(y)^2 + g) % Convert ode to vector field
